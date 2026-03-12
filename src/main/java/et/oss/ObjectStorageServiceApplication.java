f3f8ae13255b641ec6f457ea4493fada6b41e942
Erjan <tasmatoverzan05@gmail.com>
2026-03-12 16:47:15 +0600

    Проведена настройка проекта
    1) Проект подключен к GitHub репозиторию
    2) Установлены зависимости которые понадобятся в начале проекта
    3) Проведена настройка конфигураций проекта

---
+++ src/main/java/et/oss/ObjectStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package et.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectStorageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectStorageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/et/oss/ObjectStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package et.oss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectStorageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
