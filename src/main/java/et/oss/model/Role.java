938df730dc487fa4996ee1285e355d9dd305bb5b
Erjan <tasmatoverzan05@gmail.com>
2026-04-08 14:39:24 +0600

    Commit #18
    1) small fix

--- src/main/java/et/oss/model/Role.java
+++ src/main/java/et/oss/model/Role.java
@@ -9,7 +9,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "roles")
+@Table(name = "role")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
