a50a7db1c0860a5754e9a4a1139791f482b788de
Erjan <tasmatoverzan05@gmail.com>
2026-04-14 19:14:08 +0600

    Commit #28
    1) Added controllers for processing password reset requests
    
    2) UserDto was changed to RegisterDto
    
    3) The "UserAlreadyExistsException" error was added to the try catch and passed to the template to display the error

--- src/main/java/et/oss/controller/AuthController.java
+++ src/main/java/et/oss/controller/AuthController.java
@@ -1,17 +1,22 @@
 package et.oss.controller;
 
-import et.oss.dto.UserDto;
+import et.oss.dto.RegisterDto;
+import et.oss.dto.ResetPasswordDto;
+import et.oss.exceptions.ExpiredLinkException;
 import et.oss.exceptions.UserAlreadyExistsException;
+import et.oss.model.User;
 import et.oss.service.AuthService;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.io.UnsupportedEncodingException;
 
 @Controller
 @RequestMapping("/auth")
@@ -22,25 +27,77 @@ public class AuthController {
 
     @GetMapping("/register")
     public String getRegister(Model model) {
-        model.addAttribute("userDto", new UserDto());
+        model.addAttribute("registerDto", new RegisterDto());
         return "auth/auth";
     }
 
     @PostMapping("/register")
-    public String registerUser(@Valid @ModelAttribute("userDto") UserDto userDto,
-                               BindingResult bindingResult) {
+    public String registerUser(@Valid @ModelAttribute("registerDto") RegisterDto registerDto,
+                               BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "auth/auth";
         }
-        authService.registerUser(userDto);
+        try{
+            authService.registerUser(registerDto);
+        } catch (UserAlreadyExistsException e) {
+            model.addAttribute("error", e.getMessage());
+        }
         return "auth/auth";
     }
 
     @GetMapping("/login")
     public String getLogin(Model model) {
-        model.addAttribute("userDto", new UserDto());
+        model.addAttribute("registerDto", new RegisterDto());
         return "auth/auth";
     }
 
+    @GetMapping("/forgotPassword")
+    public String showForgotPasswordForm() {
+        return "auth/forgotPasswordForm";
+    }
+
+    @PostMapping("/forgotPassword")
+    public String processForgotPassword(HttpServletRequest request, Model model) {
+        try {
+            authService.makeResetPasswdLink(request);
+            model.addAttribute("message", "The link has been sent to your email.");
+        } catch (UsernameNotFoundException | UnsupportedEncodingException | MessagingException ex) {
+            model.addAttribute("error", ex.getMessage());
+        }
+        return "auth/forgotPasswordForm";
+    }
+
+    @GetMapping("/resetPassword")
+    public String showResetPasswordForm(@RequestParam String token, Model model) {
+        try {
+            authService.getByResetPasswordToken(token);
+            model.addAttribute("token", token);
+            model.addAttribute("resetPasswordDto", new ResetPasswordDto());
+        }  catch (UsernameNotFoundException | ExpiredLinkException ex) {
+            model.addAttribute("error", ex.getMessage());
+        }
+        return "auth/resetPasswordForm";
+    }
+
+    @PostMapping("/resetPassword")
+    public String processResetPassword(@Valid ResetPasswordDto resetPasswordDto,
+                                       BindingResult bindingResult,
+                                       HttpServletRequest request,
+                                       Model model) {
+        String token = request.getParameter("token");
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("token", token);
+            return "auth/resetPasswordForm";
+        }
+        String password =  resetPasswordDto.getPassword();
+        try {
+            User user = authService.getByResetPasswordToken(token);
+            authService.updatePassword(user, password);
+            model.addAttribute("message", "Password changed successfully");
+        } catch (UsernameNotFoundException | ExpiredLinkException ex) {
+            model.addAttribute("error", ex.getMessage());
+        }
+        return "auth/message";
+    }
 }
 
