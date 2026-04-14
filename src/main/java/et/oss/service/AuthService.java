10f4f1422d9ab8e9cdaa93a3471874ac65d05a1c
Erjan <tasmatoverzan05@gmail.com>
2026-04-14 19:08:54 +0600

    Commit #26
    1) UserDTO has been replaced by RegisterDTO
    
    2) Added 4 new methods that implement the password reset service logic
        2.1) updateResetPasswordToken - when a user requests a password reset, this method assigns a password reset hash and a lifetime for this hash
       2.2) getByResetPasswordToken - needed to search for a user by their hash and check the expiration date of this hash
       2.3) makeResetPasswordLink - creates data for a message with a link to reset the password and works with the EmailService class
      2.4) updatePassword - updates the user's password, resets the token and resets its lifetime

--- src/main/java/et/oss/service/AuthService.java
+++ src/main/java/et/oss/service/AuthService.java
@@ -1,9 +1,28 @@
 package et.oss.service;
 
+import et.oss.dto.RegisterDto;
 import et.oss.dto.UserDto;
+import et.oss.model.User;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.UnsupportedEncodingException;
 
 public interface AuthService {
-    void registerUser(UserDto userDto);
+
+    void registerUser(RegisterDto registerDto);
 
     UserDto getUserByEmail(String email);
+
+    @Transactional
+    void updateResetPasswordToken(String token, String email);
+
+    User getByResetPasswordToken(String token);
+
+    void updatePassword(User user, String newPassword);
+
+    void makeResetPasswdLink(HttpServletRequest request)
+            throws UsernameNotFoundException, UnsupportedEncodingException, MessagingException;
 }
--- src/main/java/et/oss/service/impl/AuthServiceImpl.java
+++ src/main/java/et/oss/service/impl/AuthServiceImpl.java
@@ -1,36 +1,46 @@
 package et.oss.service.impl;
 
+import et.oss.common.Utility;
+import et.oss.dto.RegisterDto;
 import et.oss.dto.UserDto;
+import et.oss.exceptions.ExpiredLinkException;
 import et.oss.exceptions.UserAlreadyExistsException;
 import et.oss.exceptions.UserNotFoundException;
 import et.oss.model.User;
 import et.oss.repository.UserRepository;
 import et.oss.service.AuthService;
 import et.oss.service.RoleService;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
-
     private final PasswordEncoder passwordEncoder;
     private final RoleService roleService;
-
+    private final EmailService emailService;
 
     @Override
-    public void registerUser(UserDto userDto) {
+    public void registerUser(RegisterDto registerDto) {
 
-        if (userRepository.existsByEmail(userDto.getEmail())) {
-            throw new UserAlreadyExistsException("Пользователь с такой почтой уже существует");
+        if (userRepository.existsByEmail(registerDto.getEmail())) {
+            throw new UserAlreadyExistsException("A user with this email already exists");
         }
 
         User user = User.builder()
-                .email(userDto.getEmail())
-                .password(passwordEncoder.encode(userDto.getPassword()))
+                .email(registerDto.getEmail())
+                .password(passwordEncoder.encode(registerDto.getPassword()))
                 .roleId(roleService.getRoleId())
                 .enabled(true)
                 .build();
@@ -45,11 +55,50 @@ public class AuthServiceImpl implements AuthService {
         return convertToDto(user);
     }
 
+    @Transactional
+    @Override
+    public void updateResetPasswordToken(String token, String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь с такой почтой не найден: " + email));
+        user.setResetPasswordLink(token);
+        user.setResetTokenExpiresAt(LocalDateTime.now().plusMinutes(30));
+        userRepository.saveAndFlush(user);
+    }
+
+    @Override
+    public User getByResetPasswordToken(String token) {
+        User user = userRepository.findByResetPasswordLink(token)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        if (user.getResetTokenExpiresAt().isBefore(LocalDateTime.now())) {
+            throw new ExpiredLinkException("The link has expired");
+        }
+        return user;
+    }
+
+    @Transactional
+    @Override
+    public void updatePassword(User user, String newPassword) {
+        String encodedPassword = passwordEncoder.encode(newPassword);
+        user.setPassword(encodedPassword);
+        user.setResetPasswordLink(null);
+        user.setResetTokenExpiresAt(null);
+        userRepository.saveAndFlush(user);
+    }
+
+    @Override
+    public void makeResetPasswdLink(HttpServletRequest request)
+            throws UsernameNotFoundException, UnsupportedEncodingException, MessagingException {
+        String email = request.getParameter("email");
+        String token = UUID.randomUUID().toString();
+        updateResetPasswordToken(token, email);
+        String resetPasswordLink = Utility.getSiteURL(request) + "/auth/resetPassword?token=" + token;
+        emailService.sendEmail(email, resetPasswordLink);
+    }
+
     private UserDto convertToDto(User user) {
         return UserDto.builder()
                 .id(user.getId())
                 .email(user.getEmail())
-                .password(user.getPassword())
                 .roleId(user.getRoleId())
                 .build();
     }
